1ae27d8f488b82b60c6e18f3df1b0ccae11b4f05
MellowCo <799478052@qq.com>
2022-07-30 10:38:08 +0800

    chore: add auto-import

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,19 @@
+<script setup lang="ts">
+const title = ref('Hello')
+
+onShow(() => {
+  console.log('onShow')
+})
+</script>
+
 <template>
   <view class="center flex-col">
     <image class="w-200 h-200 mt-200rpx mb-50rpx mx-auto" src="/static/logo.png" />
     <view class="flex justify-center">
-      <text class="text-lg text-[#8f8f94]">{{ title }}</text>
+      <text class="text-lg text-[#8f8f94]">
+        {{ title }}
+      </text>
     </view>
   </view>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
-</script>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,42 @@
-import { defineConfig } from "vite";
-import uni from "@dcloudio/vite-plugin-uni";
+import path from 'path'
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
 import Unocss from 'unocss/vite'
 import transformWeClass from 'unplugin-transform-we-class/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
   plugins: [
     uni(),
+
+    // https://github.com/antfu/unocss
     Unocss(),
     transformWeClass(),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          '@dcloudio/uni-app': [
+            'onLaunch',
+            'onShow',
+            'onHide',
+            'onLoad',
+            'onPullDownRefresh',
+            'onReachBottom',
+          ],
+        },
+      ],
+      dts: true,
+      vueTemplate: true,
+    }),
+
   ],
-});
+})
