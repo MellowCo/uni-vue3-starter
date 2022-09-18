de683797833175c41e5fa15db7b02129c1b817a6
MellowCo <799478052@qq.com>
2022-09-18 17:01:26 +0800

    chore(config): update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -14,22 +14,8 @@ export default defineConfig({
   plugins: [
     uni(),
 
-    // https://github.com/MellowCo/unplugin-unocss-attributify-wechat
-    presetAttributifyWechat(),
-
     // https://github.com/antfu/unocss
     Unocss(),
-    // Unocss(),
-
-    // app打包配置
-    // uniapp打包app时，打包2次，一次使用 vue 模式打包h5，第2次使用 nvue 模式打包app，
-    // 第2次打包 unocss 会抛出warn
-    // entry module not found, have you add `import 'uno.css'` in your main entry?
-    // 导致打包终止
-    process.env.UNI_COMPILER !== 'nvue' ? Unocss() : undefined,
-
-    // https://github.com/MellowCo/unplugin-transform-we-class
-    transformWeClass(),
 
     // https://github.com/antfu/unplugin-auto-import
     AutoImport({
